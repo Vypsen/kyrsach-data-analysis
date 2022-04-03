09e8495be695412a94527dabff77f89b02270e7b
Vypsen <s.semenets2001@mail.ru>
2022-04-03 23:27:36 +1000

    create chiSquare func, fix empty value

---
+++ helpFunc.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+def chiSquare(data):
+    statistical_significance = 0.05
+    chi_square = 0
+    critical_value = 3.84
+    value = np.array([data.iloc[0][0:-1].values,
+                      data.iloc[1][0:-1].values,
+                      data.iloc[2][0:-1].values])
+                      
+    total = value[value.shape[0] - 1][value.shape[0] - 1]
+
+    df = (value.shape[0] - 1 - 1)*(value.shape[0] - 1 - 1)
+
+    for i in range(value.shape[0] - 1):
+        for j in range(value.shape[0] - 1):
+            expected_value = (value[i][value.shape[0] - 1]*value[value.shape[0] - 1][j])/total
+            chi_square += (value[i][j] - expected_value)**2/expected_value
+
+    return chi_square
