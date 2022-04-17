85f9164850244c77dfb6e5214ac20dd0a05e750d
Vypsen <s.semenets2001@mail.ru>
2022-04-18 02:21:29 +1000

    fix table

--- funcs/chiSquare.py
+++ funcs/chiSquare.py
@@ -5,13 +5,11 @@ def chiSquare(data):
     statistical_significance = 0.05
     chi_square = 0
     critical_value = 3.84
-    value = np.array([data.iloc[0][0:-1].values,
-                      data.iloc[1][0:-1].values,
-                      data.iloc[2][0:-1].values])
-                      
-    total = value[value.shape[0] - 1][value.shape[0] - 1]
-
+    value = np.array([data.iloc[0][0:4].values,
+                      data.iloc[1][0:4].values,
+                      data.iloc[2][0:4].values])
     df = (value.shape[0] - 1 - 1)*(value.shape[0] - 1 - 1)
+    total = value[value.shape[0] - 1][value.shape[0] - 1]
 
     for i in range(value.shape[0] - 1):
         for j in range(value.shape[0] - 1):
